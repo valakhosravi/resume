c452ceda31f19e64ce3f8c6f0c343b790e33c41d
Vala Khosravi <valakhosravi0@gmail.com>
2024-02-10 14:38:11 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  distDir: "dist"
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
